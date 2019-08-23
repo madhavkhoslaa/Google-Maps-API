2b6d4daa44953464e8d4c4094c73e329415685de
Madhav Khosla <28945483+madhavkhoslaa@users.noreply.github.com>
2019-08-23 15:35:59 +0530

    Added radius argument in the reverse geocode

--- Googlebox.py
+++ Googlebox.py
@@ -9,6 +9,8 @@ from area import area
 from skimage import io
 from tqdm import tqdm
 import requests
+import os
+
 
 class API():
     def __init__(self, key, AreaLatLongBound, MapZoom, ImageSize):
@@ -20,45 +22,54 @@ class API():
             list)
         assert isinstance(ImageSize, str)
         """AreaLatLongBound=(topLeftLat, topLeftLong, bottomRightLat, bottomRightLong)"""
-        self.key= key
-        self.AreaLatLongBound= tuple(AreaLatLongBound)
+        self.key = key
+        self.AreaLatLongBound = tuple(AreaLatLongBound)
         self.MapZoom = str(MapZoom)
         self.ImageSize = ImageSize
         self.UniqueContours = list()
         self.RejectedContours = list()
         self.AllContour = list()
         self.SmallContours = list()
-        self.query_pts = self.createQueryFragments(self.AreaLatLongBound[0], self.AreaLatLongBound[1], self.AreaLatLongBound[2], self.AreaLatLongBound[3])
-        self.faddr= []
-        self.geo= []
+        self.query_pts = self.createQueryFragments(
+            self.AreaLatLongBound[0],
+            self.AreaLatLongBound[1],
+            self.AreaLatLongBound[2],
+            self.AreaLatLongBound[3])
+        self.faddr = []
+        self.geo = []
         self.centroids = []
         self.unique_all_contours = []
-        self.all_contours_list= []
-        self.clat_clong_contours= dict()
+        self.all_contours_list = []
+        self.clat_clong_contours = dict()
 
-
-    def createQueryFragments(self, topLeftLat, topLeftLong, bottomRightLat, bottomRightLong):
+    def createQueryFragments(
+            self, topLeftLat, topLeftLong, bottomRightLat, bottomRightLong):
         down = 0.005900
         right = 0.006600
         mar = 0.0002
-        return np.mgrid[topLeftLat - (down/2):bottomRightLat:-0.005700, topLeftLong + (right/2):bottomRightLong:0.006400].reshape(2, -1).T
+        return np.mgrid[topLeftLat - (down / 2):bottomRightLat:-0.005700, topLeftLong + (
+            right / 2):bottomRightLong:0.006400].reshape(2, -1).T
 
     def white_image(self, im):
         # returns a white image of the shape input image
         return cv2.bitwise_not(np.zeros(im.shape, np.uint8))
-    def reversegeocode(self, lat, long_):
-        base= "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}".format(str(lat), str(long_), self.key)
+
+    def reversegeocode(self, lat, long_, radius):
+        base = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&radius={}&key={}".format(
+            str(lat), str(long_), str(radius),self.key)
         r = requests.get(base).json()
         return r
+
     def distance_lat_long(self, point1, point2):
         R = 6373.0
-        point1= list(map(math.radians, point1))
-        point2= list(map(math.radians, point2))
-        dlat= math.radians(point1[0]- point2[0])
-        dlon= math.radians(point1[1]- point2[1])
-        a = math.sin(dlat / 2)**2 + math.cos(point1[0]) * math.cos(point2[0]) * math.sin(dlon / 2)**2
+        point1 = list(map(math.radians, point1))
+        point2 = list(map(math.radians, point2))
+        dlat = math.radians(point1[0] - point2[0])
+        dlon = math.radians(point1[1] - point2[1])
+        a = math.sin(
+            dlat / 2)**2 + math.cos(point1[0]) * math.cos(point2[0]) * math.sin(dlon / 2)**2
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-        return R* c* 1000
+        return R * c * 1000
 
     def area_frm_cnt(self, cnt):
         # finds the area of a contour
@@ -72,18 +83,19 @@ class API():
         img = color.gray2rgb(img)
         coordinates = np.array(coordinates)
         img[coordinates[:, 0], coordinates[:, 1]] = clr
-        arr = coordinates.sum(axis = 0).reshape((2, 1))/coordinates.shape[0]
+        arr = coordinates.sum(axis=0).reshape((2, 1)) / coordinates.shape[0]
         img[int(arr[0]), int(arr[1])] = clr
         return img
 
     @classmethod
     def is_arr_in_list(self, myarr, list_arrays):
         return next((True for elem in list_arrays if elem is myarr), False)
+
     @classmethod
-    def arrayCnt2area(self,l):
+    def arrayCnt2area(self, l):
         # l is the name of nX2 array
-        return area({'type':'Polygon','coordinates':[l.tolist()]})
-    
+        return area({'type': 'Polygon', 'coordinates': [l.tolist()]})
+
     def Centroid(self, arr):
         if not isinstance(arr, np.ndarray):
             arr = np.array(arr)
@@ -125,18 +137,21 @@ class API():
                     center_latitude,
                     center_longitude))
         return arr
-        
+
     def updatekey(self, key):
         assert isinstance(key, str)
-        self.key= key
+        self.key = key
 
     def getboundries(self, center_latitude, center_longitude, save=True):
-        self.center_latitude= center_latitude
-        self.center_longitude= center_longitude
+        self.center_latitude = center_latitude
+        self.center_longitude = center_longitude
         str_Center = str(center_latitude) + ',' + str(center_longitude)
         safeURL_Style = quote(
             'feature:landscape.man_made|element:geometry.stroke|visibility:on|color:0xffffff|weight:1')
-        urlBuildings = "http://maps.googleapis.com/maps/api/staticmap?center=" + str_Center + "&zoom=" + self.MapZoom + "&format=png32&sensor=false&size=" + self.ImageSize + "&maptype=roadmap&style=visibility:off&style=" + safeURL_Style + '&key='+ str(self.key)
+        urlBuildings = "http://maps.googleapis.com/maps/api/staticmap?center=" + str_Center + "&zoom=" + self.MapZoom + \
+            "&format=png32&sensor=false&size=" + self.ImageSize + \
+            "&maptype=roadmap&style=visibility:off&style=" + \
+            safeURL_Style + '&key=' + str(self.key)
         boundry_image = skimage.color.rgb2gray(skimage.io.imread(urlBuildings))
         self.Img = boundry_image
         boundry_image = np.where(
@@ -151,19 +166,23 @@ class API():
         self.AllContour.extend(contoursBuildings)
         if save:
             skimage.io.imsave(
-                "Skel/CentreLatLong:{}MapZoom:{}.png".format(
+                "CentreLatLong:{}MapZoom:{}.png".format(
                     str_Center,
                     self.MapZoom),
                 boundry_image_raw)
         else:
             pass
-        self.clat_clong_contours.update({(center_latitude, center_longitude): contoursBuildings})
+        self.clat_clong_contours.update(
+            {(center_latitude, center_longitude): contoursBuildings})
         return boundry_image_raw
 
-    def getImage(self, clat, clong, save= True):
-        base= "https://maps.googleapis.com/maps/api/staticmap?center={},+{}6&zoom={}&scale=1&size={}&maptype=satellite&format=png&visual_refresh=true&key={}".format(clat, clong, self.MapZoom, self.ImageSize, self.key)
-        print(base)
-        Img= skimage.io.imread(base)
-        if save== True:
-            skimage.io.imsave(fname= "Sat/satellite Lat:{} Long:{}.png".format(clat, clong), arr= Img)
+    def getImage(self, clat, clong, save=True):
+        base = "https://maps.googleapis.com/maps/api/staticmap?center={},+{}6&zoom={}&scale=1&size={}&maptype=satellite&format=png&visual_refresh=true&key={}".format(
+            clat, clong, self.MapZoom, self.ImageSize, self.key)
+        Img = skimage.io.imread(base)
+        if save == True:
+            skimage.io.imsave(
+                fname="satellite Lat:{} Long:{}.png".format(
+                    clat, clong), arr=Img)
+        # AIzaSyA7mOyo5rgqI8-vWa6IKkgOvlIpt3Mn6Tg
         return Img
